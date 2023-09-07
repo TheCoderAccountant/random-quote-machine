46471a6d51169231fcdb3d6a0214b7fd56c76b68
Nurlan Khammadli <nurlanxm@gmail.com>
2023-09-07 16:33:16 +0400

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
-const apiKey = "qxZ7U0oPzXfxaqwMCOhQIQ==zwNXkcSrD8Gd8d6j";
+const apiKey = "A42Y6nel30/KxXYBml+s9A==CFKrp5TVUt8uQl8h";
 
 const list = ['age', 'alone', 'amazing', 'anger', 'architecture', 'art', 'attitude', 'beauty', 'best', 'birthday', 'business'];
 const mainColors = ['red', 'blue', 'green', 'orange', 'purple', '#902C3E', 'teal', 'brown', 'gray'];
 
 function randomColor() {
-  return Math.floor(Math.random() * mainColors.length);
+  const randomIndex = Math.floor(Math.random() * mainColors.length);
+  return randomIndex;
 }
 
 function App() {
@@ -19,13 +20,14 @@ function App() {
 
   useEffect(() => clickHandler, [])
   useEffect(() => setColorIndex(randomColor()), [state])
-
+  useEffect(() => {
+    document.body.style.backgroundColor = mainColors[colorIndex];
+  }, [colorIndex]);
 
   function fetchQuote() {
     const randomCat = Math.floor(Math.random() * list.length)
     const category = list[randomCat]
-
-    axios.get('https://api.api-ninjas.com/v1/quotes?category=' + category, {
+    return axios.get('https://api.api-ninjas.com/v1/quotes?category=' + category, {
       headers: { 'X-Api-Key': apiKey }
     })
       .then(response => {
@@ -52,16 +54,27 @@ function App() {
   return (
 
     <div id="quote-box"  >
-      <p id='text' style={fontStyles}>
+      <p
+        id='text'
+        style={fontStyles}>
         <i className="fa fa-quote-left"> </i>
         {state.quote ? " " + state.quote : ""}
       </p>
-      <p id='author' style={fontStyles}>
+      <p
+        id='author'
+        style={fontStyles}>
         {state.author ? `- ` + state.author : ''}
       </p>
       <div className='bottom-line'>
-        <a id="tweet-quote" href='https://twitter.com/intent/tweet' target="_blank" style={btnStyles}><i style={{ color: "white" }} className="fab fa-twitter"></i></a>
-        <button id="new-quote" style={btnStyles} onClick={clickHandler}>
+        <a id="tweet-quote"
+          href='https://twitter.com/intent/tweet'
+          target="_blank"
+          style={btnStyles}>
+          <i style={{ color: "white" }} className="fab fa-twitter"></i>
+        </a>
+        <button id="new-quote"
+          style={btnStyles}
+          onClick={clickHandler}>
           New Quote
         </button>
       </div>
