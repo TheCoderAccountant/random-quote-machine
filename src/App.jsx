c7f3f2c9cefb981f9698e90fcb84e07d07a6944a
Nurlan Khammadli <nurlanxm@gmail.com>
2023-09-07 14:49:57 +0400

    bugs fixes color added

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,70 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
-const list = ['age', 'alone', 'amazing', 'anger', 'architecture', 'art', 'attitude', 'beauty', 'best', 'birthday', 'business'];
+const apiKey = "qxZ7U0oPzXfxaqwMCOhQIQ==zwNXkcSrD8Gd8d6j";
 
-function randomCat() {
-  return Math.floor(Math.random() * list.length);
+const list = ['age', 'alone', 'amazing', 'anger', 'architecture', 'art', 'attitude', 'beauty', 'best', 'birthday', 'business'];
+const mainColors = ['red', 'blue', 'green', 'orange', 'purple', '#902C3E', 'teal', 'brown', 'gray'];
+
+function randomColor() {
+  return Math.floor(Math.random() * mainColors.length);
 }
 
-const apiKey = "qxZ7U0oPzXfxaqwMCOhQIQ==zwNXkcSrD8Gd8d6j";
-const category = list[randomCat()]
-
-
 function App() {
-  const [state, setState] = useState([])
 
-  const clickHandler = () => {
+  const [state, setState] = useState([])
+  const [colorIndex, setColorIndex] = useState(0);
+
+
+  useEffect(() => clickHandler, [])
+  useEffect(() => setColorIndex(randomColor()), [state])
+
+
+  function fetchQuote() {
+    const randomCat = Math.floor(Math.random() * list.length)
+    const category = list[randomCat]
+
     axios.get('https://api.api-ninjas.com/v1/quotes?category=' + category, {
       headers: { 'X-Api-Key': apiKey }
     })
       .then(response => {
-        setState(response.data);
+        setState(response.data[0]);
       })
       .catch(error => {
-        console.error('Request failed:', error);
-      });
+        console.error('Request failed:', error)
+      })
   }
 
-  useEffect(() => {
-    return clickHandler;
-  }, []);
+
+  const clickHandler = () => {
+    fetchQuote();
+    setColorIndex(randomColor());
+  }
+
+  const btnStyles = {
+    backgroundColor: mainColors[colorIndex],
+    color: "white",
+  }
+  const fontStyles = { color: mainColors[colorIndex] }
+
 
   return (
-    <>
-      <div id="quote-box">
 
-        <p id='text'>
-          {state.length > 0 ? `"` + state[0].quote : '"'}
-        </p>
-        <p id='author'>
-          {state.length > 0 ? `- ` + state[0].author : ''}
-          {state.length > 0 ? `- ` + state[0].category : ''}
-        </p>
-        <button id="new-quote" onClick={clickHandler}>
+    <div id="quote-box"  >
+      <p id='text' style={fontStyles}>
+        <i className="fa fa-quote-left"> </i>
+        {state.quote ? " " + state.quote : ""}
+      </p>
+      <p id='author' style={fontStyles}>
+        {state.author ? `- ` + state.author : ''}
+      </p>
+      <div className='bottom-line'>
+        <a id="tweet-quote" href='https://twitter.com/intent/tweet' target="_blank" style={btnStyles}><i style={{ color: "white" }} className="fab fa-twitter"></i></a>
+        <button id="new-quote" style={btnStyles} onClick={clickHandler}>
           New Quote
         </button>
       </div>
-    </>
+    </div >
   )
 }
 
